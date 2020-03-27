754b8c528f044d35d8dc1eba735b2f00a80818af
Gremlin4544 <alve@mckethen.com>
2020-03-27 13:41:10 -0400

    created client with CRA

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -19,6 +19,6 @@ module.exports = (req, res, next) => {
       }
     });
   } else {
-    res.status(401).json({ you: 'shall not pass!' });
+    res.status(401).json({ you: 'authentication failed - shall not pass!' });
   }
 };
