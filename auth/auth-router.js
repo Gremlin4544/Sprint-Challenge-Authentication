f325fc0708974cf229ed11966d9e080ea9c90e8a
Gremlin4544 <alve@mckethen.com>
2020-03-27 11:53:28 -0400

    installed bcryptjs and implemented registration on auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  User.addUser(user)
+      .then(saved => {
+        const token = generateToken(user);
+        res.status(201).json({user: saved, token: token})
+      })
+      .catch(err => {
+        res.status(500).json({message: 'Failed to add user'})
+      });
 });
 
 router.post('/login', (req, res) => {
